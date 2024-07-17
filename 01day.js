e7d34d4bc73c94f3aaf92a728da3ec9fe8929d22
Aftab073 <tamboliaftab84@gmail.com>
2024-07-17 20:42:51 +0530

    Variable & Data Types

---
+++ 01day.js
@@ -0,0 +1,50 @@
+// Variables and Data Types
+
+// Activity 1: Variable Declaration
+// Task 1: Assign 'number' using "var" and console
+var num = 7
+console.log(num);
+
+// Task 2: Assign 'string' using "let" and console
+let str = "Aftab"
+console.log(str);
+
+
+//Activity 2: Constant declaration
+//Task 3: Assign 'boolean vale' using "const"
+const boolVal = true
+console.log(boolVal);
+
+// Activity 3: Data Types
+// Task 4:Variable of diff types and console there types
+
+let number = 10;
+let string = "Aftab";
+let bool_value = false;
+const obj = { name: "Tamboli", age: 22, gender: "M" };
+const arr = [1, 2, 3, 4, 5];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof bool_value);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4: Reassigning Variable
+
+// Task 5: Asssign value using 'let', reassign new value and log both values
+
+let initial = 70;
+console.log(initial);
+initial = 200;
+console.log(some_var);
+
+// Activity 5: Understanding const
+
+// Task 6: Try reassigning a variable declared with const
+
+const unChanged = 73;
+console.log(unChanged);  // Output: 300
+
+unChanged = 57;  // Error: Assignment to constant variable
+console.log(unChanged);
