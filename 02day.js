5a3ca42e951aa9d4738d55cde42e68f13fe4acb5
Aftab073 <tamboliaftab84@gmail.com>
2024-07-17 21:12:28 +0530

    Operators

---
+++ 02day.js
@@ -0,0 +1,77 @@
+//Day 02: Operators
+
+// Activity 1: Arithmetic Operations
+// Task 1:Addition
+let a = 5;
+let b = 7;
+console.log(a + b);
+
+// Task 2: Substraction
+let c = 5;
+let d = 7;
+console.log(c - d);
+
+// Task 3: Multiplication
+let e = 5;
+let f = 7;
+console.log(e * f);
+
+// Task 4:Division
+let g = 5;
+let h = 7;
+console.log(g / h);
+
+// Task 5: finding reminder
+let i = 5;
+let j = 7;
+console.log(i % j);
+
+
+// Activity 2: Assignment Operators
+
+// Task 6: += operator to add number
+let num = 5;
+num += 10;
+console.log(num);
+
+// Task 7: -= operator to substract number
+let num2 = 20;
+num2 -= 5;
+console.log(num2);
+
+
+// Activity 3: Comparison Operators
+// Task 8: Compare two nmbrs
+let comp1 = 10
+let comp2 = 5
+console.log(comp1 > comp2); //op : true
+console.log(comp1 < comp2); //op : false
+
+
+
+// Task 9: Compare ttwo nmbrs using >= , <=
+console.log(comp1 >= comp2); //op : true
+console.log(comp1 <= comp2); //op : true
+
+
+// Task 10:
+
+console.log(10 == 10); //op : true
+console.log(10 === 10); //op : true
+
+
+// Activity 4: Logical Operators
+// Task 11: Both conditions should be true
+console.log(10 > 7 && 7 < 10);
+
+// Task 12: Either one condition should true
+console.log(10 > 5 || 20 < 10);
+
+// Task 13: ! = not
+console.log(!10 == 5);
+
+
+// Activity 5: Ternary Operators
+// Task 14:
+
+10 < 7 ? console.log("Number is Negative") : console.log("Number is positive");
